d53025662694d5192ad0f546812f4aca458dcc6f
doraparia <144099886+doraparia@users.noreply.github.com>
2023-09-05 20:25:42 +0530

    Update streamlit_app.py
    new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-import streamlit
-streamlit.title('My parents new healthy diner')
+import streamlit 
+streamlit.title('My parents New Healthy Diner' ) 
+streamlit.header( 'Breakfast Menu') 
+streamlit.text( 'Omega 3 g Blueberry Oatmeal' ) 
+streamlit.text( 'Kale, Spinach & Rocket Smoothie') 
+streamlit.text( 'Hard-Boiled Free-Range Egg') 
